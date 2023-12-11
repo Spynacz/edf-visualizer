b7864f7bb75197dea92931cb19b9542b2cc7c137
YaBoyya <duzniak.amadeusz@gmail.com>
2023-12-11 13:31:40 +0100

    Add NoArgsConstructor and all getters to User class

--- server/src/main/java/org/fhdmma/edf/User.java
+++ server/src/main/java/org/fhdmma/edf/User.java
@@ -1,10 +1,13 @@
 package org.fhdmma.edf;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
 public class User {
-    @Getter private int id;
-    @Getter private String username;
+    private int id; 
+    private String username;
     private String password;
 
     public User(int id, String username, String password) {
