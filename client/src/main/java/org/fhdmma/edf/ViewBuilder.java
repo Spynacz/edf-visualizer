fdb2cbebf7981d3cb84879f8262416d82371b45c
Spynacz <63506536+Spynacz@users.noreply.github.com>
2023-12-16 16:06:14 +0100

    Refactor new task dialog

--- client/src/main/java/org/fhdmma/edf/ViewBuilder.java
+++ client/src/main/java/org/fhdmma/edf/ViewBuilder.java
@@ -1,10 +1,13 @@
 package org.fhdmma.edf;
 
+import java.text.NumberFormat;
 import java.util.Objects;
 import java.util.function.Consumer;
 
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.StringProperty;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -12,6 +15,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
@@ -61,7 +65,7 @@ public class ViewBuilder implements Builder<Region> {
         vbox.getChildren().add(setClientTasks(taskDetailsDisplayer));
         VBox.setVgrow(vbox.getChildren().get(0), Priority.ALWAYS);
 
-        vbox.getChildren().add(setAddTaskButton(taskAdder));
+        vbox.getChildren().add(setAddTaskButton());
 
         vbox.getStyleClass().add("leftbox");
         return vbox;
@@ -129,53 +133,133 @@ public class ViewBuilder implements Builder<Region> {
                 boundLabel(model.selectedPeriodProperty()));
     }
 
-    private Node setAddTaskButton(Consumer<Runnable> addTask) {
+    private Node setAddTaskButton() {
         Button button = new Button("Add task");
 
         button.setOnAction(evt -> {
             final Stage dialog = new Stage();
             dialog.initModality(Modality.APPLICATION_MODAL);
 
-            HBox title = new HBox(6, new Label("Task name:"), boundTextField(model.titleProperty()));
-            HBox duration = new HBox(6, new Label("Task duration:"),
-                    boundIntegerField(model.durationProperty()));
-            HBox period = new HBox(6, new Label("Task period:"),
-                    boundIntegerField(model.periodProperty()));
-            Button confirm = new Button("Confirm");
-            confirm.setOnAction(evt2 -> {
-                addTask.accept(() -> dialog.close());
-            });
+            BorderPane borderPane = new BorderPane();
+            Node center = setAddTaskDialog(taskAdder, dialog);
+            borderPane.setCenter(center);
+            BorderPane.setMargin(center, new Insets(50));
 
-            VBox vbox = new VBox(title, duration, period, confirm);
-
-            Scene dialogScene = new Scene(vbox, 400, 300);
+            Scene dialogScene = new Scene(borderPane);
             dialog.setScene(dialogScene);
+            dialog.setResizable(false);
             dialog.show();
         });
         return button;
     }
 
-    private Node boundTextField(StringProperty boundProperty) {
+    private Node setAddTaskDialog(Consumer<Runnable> addTask, Stage stage) {
+        Node periodText = boundIntegerField(model.periodProperty(), "Period");
+        Node durationText = boundIntegerField(model.durationProperty(), "Duration", periodText);
+        Node titleText = boundTextField(model.titleProperty(), "Title", durationText);
+
+        HBox title = new HBox(6, new Label("Title:"), hboxSpacer(), titleText);
+        HBox duration = new HBox(6, new Label("Duration:"), hboxSpacer(), durationText);
+        HBox period = new HBox(6, new Label("Period:"), hboxSpacer(), periodText);
+
+        Button confirm = new Button("Confirm");
+        confirm.setOnAction(evt2 -> {
+            addTask.accept(() -> stage.close());
+        });
+
+        confirm.setDefaultButton(true);
+
+        VBox vbox = new VBox(10, title, duration, period, confirm);
+        vbox.setAlignment(Pos.CENTER);
+
+        return vbox;
+    }
+
+    private Region hboxSpacer() {
+        Region region = new Region();
+        HBox.setHgrow(region, Priority.ALWAYS);
+        return region;
+    }
+
+    private Node boundTextField(StringProperty boundProperty, String prompt) {
         TextField textField = new TextField();
         textField.textProperty().bindBidirectional(boundProperty);
+        textField.setPromptText(prompt);
+
         return textField;
     }
 
-    private Node boundIntegerField(IntegerProperty boundProperty) {
+    private Node boundTextField(StringProperty boundProperty, String prompt, Node nextField) {
         TextField textField = new TextField();
-        textField.textProperty().bindBidirectional(boundProperty, new NumberStringConverter());
+        textField.textProperty().bindBidirectional(boundProperty);
+        textField.setPromptText(prompt);
+        textField.setOnKeyPressed(evt -> {
+            if (evt.getCode().equals(KeyCode.ENTER)) {
+                nextField.requestFocus();
+            }
+        });
+
+        return textField;
+    }
+
+    private Node boundIntegerField(IntegerProperty boundProperty, String prompt) {
+        TextField textField = new TextField();
+        textField.textProperty().bindBidirectional(boundProperty, new NumberStringConverter() {
+
+            @Override
+            public String toString(Number value) {
+                if (value == null || value.intValue() == 0) {
+                    return "";
+                }
+
+                NumberFormat formatter = getNumberFormat();
+
+                return formatter.format(value);
+            }
+        });
+        textField.setPromptText(prompt);
+
+        return textField;
+    }
+
+    private Node boundIntegerField(IntegerProperty boundProperty, String prompt, Node nextField) {
+        TextField textField = new TextField();
+        textField.textProperty().bindBidirectional(boundProperty, new NumberStringConverter() {
+
+            @Override
+            public String toString(Number value) {
+                if (value == null || value.intValue() == 0) {
+                    return "";
+                }
+
+                NumberFormat formatter = getNumberFormat();
+
+                return formatter.format(value);
+            }
+        });
+
+        textField.setPromptText(prompt);
+
+        textField.setOnKeyPressed(evt -> {
+            if (evt.getCode().equals(KeyCode.ENTER)) {
+                nextField.requestFocus();
+            }
+        });
+
         return textField;
     }
 
     private Node boundLabel(StringProperty boundProperty) {
         Label label = new Label();
         label.textProperty().bind(boundProperty);
+
         return label;
     }
 
     private Node headingLabel(String string) {
         Label label = new Label("EDF Visualizer");
         label.getStyleClass().add("main-label");
+
         return label;
     }
 }
